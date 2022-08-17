82e0c17a040630c772ae5e96aeb6f7cb73e56cfc
xndcmd <xndcmd@gmail.com>
2022-08-17 12:30:35 -0700

    added synth tones

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 // --------------------------------- \\
 // -------- Javascript Page -------- \\
 // --------------------------------- \\
+
+
+const keys = document.querySelectorAll('.key');
+const regulars = document.querySelectorAll('.key.white');
+const sharps = document.querySelectorAll('.key.black');
+
+
+keys.forEach(key => {
+    key.addEventListener('click', () => playNote(key))
+})
+
+
+// -- click to play audio function
+let playNote = (key) => {
+    const noteSound = document.getElementById(key.dataset.note)
+    noteSound.currentTime = 0
+    noteSound.play()
+    key.classList.add('active')
+    noteSound.addEventListener('ended', () => {
+        key.classList.remove('active')
+    })
+}
