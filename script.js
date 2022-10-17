5c361a3ff5121b838fbd38287ce9378b1224c6a7
xndcmd <xndcmd@gmail.com>
2022-10-17 13:29:46 -0700

    added desktop keydown music note play functionality

--- script.js
+++ script.js
@@ -7,9 +7,20 @@ const keys = document.querySelectorAll('.key');
 const regulars = document.querySelectorAll('.key.white');
 const sharps = document.querySelectorAll('.key.black');
 
+const whitekey = ['a', 's', 'd'];
 
+// -- play via mobile and/or mouse click -- //
 keys.forEach(key => {
     key.addEventListener('click', () => playNote(key))
+})
+
+// -- play via desktop keyboard -- //
+document.addEventListener('keydown', e => {
+    if (e.repeat) return
+    const key = e.key
+    const whiteKeyIndex = whitekey.indexOf(key)
+
+    if (whiteKeyIndex > -1) playNote(regulars[whiteKeyIndex])
 })
 
 
