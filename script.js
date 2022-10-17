874f7b941abfdbf0fa035df1a92e0cee711e3fef
xndcmd <xndcmd@gmail.com>
2022-10-17 13:43:45 -0700

    all black/white keys now linked and playable via keyboard

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // --------------------------------- \\
-// -------- Javascript Page -------- \\
+// -------- JavaScript Page -------- \\
 // --------------------------------- \\
 
 
@@ -7,30 +7,33 @@ const keys = document.querySelectorAll('.key');
 const regulars = document.querySelectorAll('.key.white');
 const sharps = document.querySelectorAll('.key.black');
 
-const whitekey = ['a', 's', 'd'];
+const whitekey = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'];
+const blackkey = ['w', 'e', 't', 'y', 'u', 'o', 'p'];
 
 // -- play via mobile and/or mouse click -- //
 keys.forEach(key => {
     key.addEventListener('click', () => playNote(key))
-})
+});
 
 // -- play via desktop keyboard -- //
 document.addEventListener('keydown', e => {
-    if (e.repeat) return
-    const key = e.key
-    const whiteKeyIndex = whitekey.indexOf(key)
+    if (e.repeat) return; 
+    const key = e.key;
 
-    if (whiteKeyIndex > -1) playNote(regulars[whiteKeyIndex])
-})
+    const whiteKeyIndex = whitekey.indexOf(key);
+    const blackKeyIndex = blackkey.indexOf(key);
 
+    if (whiteKeyIndex > -1) playNote(regulars[whiteKeyIndex]);
+    if (blackKeyIndex > -1) playNote(sharps[blackKeyIndex]);
+});
 
-// -- click to play audio function
+// -- click to play audio function -- //
 let playNote = (key) => {
-    const noteSound = document.getElementById(key.dataset.note)
-    noteSound.currentTime = 0
-    noteSound.play()
-    key.classList.add('active')
+    const noteSound = document.getElementById(key.dataset.note);
+    noteSound.currentTime = 0;
+    noteSound.play();
+    key.classList.add('active');
     noteSound.addEventListener('ended', () => {
         key.classList.remove('active')
-    })
-}
+    });
+};
